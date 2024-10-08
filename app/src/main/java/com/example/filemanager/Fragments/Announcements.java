aacb38cc3174eec3212e402783fe73538f858142
WhiteCat2023 <berndtdenniscanaya@gmail.com>
2024-10-08 13:44:16 +0800

    ADDED THE ANNOUNCEMENTS PAGE

---
+++ app/src/main/java/com/example/filemanager/Fragments/Announcements.java
@@ -0,0 +1,23 @@
+package com.example.filemanager.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.filemanager.R;
+
+
+public class Announcements extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_announcements, container, false);
+
+        return view;
+    }
+}
